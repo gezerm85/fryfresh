554e46b7ad039e5f4490fe1bce9edcef49da4c74
“muratseven” <muratseven50@gmail.com>
2024-09-08 21:32:31 +0300

    update 2

--- src/screens/RecipeDetailScreen.js
+++ src/screens/RecipeDetailScreen.js
@@ -19,6 +19,7 @@ export default function RecipeDetailScreen(props) {
     const [meal, setMeal] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    //test
     useEffect(()=>{
         getMealData(item.idMeal);
     },[])
